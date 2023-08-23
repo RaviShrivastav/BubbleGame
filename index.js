f22ff9157c5b40d2b870551dc85de7185f75633f
Ravi Shrivastav <ravishrivastav1126@gmail.com>
2023-08-23 11:41:11 +0530

    bubbleGame

---
+++ index.js
@@ -0,0 +1,44 @@
+var timer = 60;
+var score = 0;
+var hitRn = 0;
+
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+}
+function getNewHit() {
+  hitRn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitval").textContent = hitRn;
+}
+function makeBubble() {
+  var clutter = "";
+
+  for (i = 1; i <= 102; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}</div>`;
+  }
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+function runTimer() {
+  var timerInt = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+      clearInterval(timerInt);
+      document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+    }
+  }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (details) {
+  var clickedNum = Number(details.target.textContent);
+  if (clickedNum === hitRn) {
+    increaseScore();
+    makeBubble();
+    getNewHit();
+  }
+});
+getNewHit();
+runTimer();
+makeBubble();
